20779638edfd3ec493c5e259b611138792852de8
jpquesa8 <j.p.quesa8@gmail.com>
2026-07-17 14:40:54 -0600

    Update materiales.py
    Agregar materiales iniciales

--- precios/materiales.py
+++ precios/materiales.py
@@ -1,1 +1,18 @@
-
+MATERIALES = {
+    "PLA": {
+        "busqueda": "filamento PLA",
+        "unidad": "gramos"
+    },
+    "PLA_SILK": {
+        "busqueda": "filamento PLA Silk",
+        "unidad": "gramos"
+    },
+    "PETG": {
+        "busqueda": "filamento PETG",
+        "unidad": "gramos"
+    },
+    "RESINA_UV": {
+        "busqueda": "resina impresora 3D",
+        "unidad": "ml"
+    }
+}
